7cdd1b9637febad18245a88291daa06531412a5e
jesusinhoo <jesus.bojorquez1110@gmail.com>
2024-11-08 14:54:09 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_Implementacion_JEVB_2/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVM_Implementacion_JEVB_2.Vista;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM_Implementacion_JEVB_2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Pagina1());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVM_Implementacion_JEVB_2/VistaModelo/VMpagina1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_Implementacion_JEVB_2.VistaModelo
+{
+    public class VMpagina1 : BaseViewModel
+    {
+        #region VARIABLES
+        string _Mensaje;
+        #endregion
+        #region CONTRUCTOR
+        public VMpagina1(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region OBJETOS
+
+        public string Mensaje
+        {
+            get { return _Mensaje; }
+            set { SetValue(ref _Mensaje, value); }
+        }
+        #endregion
+        #region PROCESOS
+
+        public async Task Alerta()
+        {
+            await DisplayAlert("Titulo",Mensaje,"ok");
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand Alertacommand => new Command(async () => await Alerta());
+        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
